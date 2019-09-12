349b3511d3905c0f0960010e19d83343f65d006b
khishgee2156 <stkhishgee.2156@gmail.com>
2019-09-12 17:30:34 +0530

    Git practice added gitignore

--- Hello.java
+++ Hello.java
@@ -7,4 +7,7 @@ class Hello {
     public static int add(int x, int y){
         return x + y;
     }
+
+    public static double subtract(double x, double y){
+        return x > y ? x - y : y - x;
 }
