10c5205bda7ae1fe6306b761fc44d0be878463fb
khishgee2156 <stkhishgee.2156@gmail.com>
2019-09-12 17:22:39 +0530

    Git pratice added Method

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 class Hello {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println(add(2, 3));
+    }
+
+    public static int add(int x, int y){
+        return x + y;
     }
 }
